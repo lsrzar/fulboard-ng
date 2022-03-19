d0f86707975759aeb3e13ebbe45d6dfdc1344daf
lesrz <suarezarnaldi.leo@gmail.com>
2022-03-19 00:00:30 -0300

    Abstract sider menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+interface menuItem {
+  title: string;
+  icon: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   isCollapsed = false;
+  menuItems: menuItem[] = [
+    {
+      title: 'Dashboard',
+      icon: 'dashboard',
+    },
+    {
+      title: 'Fixtures',
+      icon: 'calendar',
+    },
+    {
+      title: 'Noticias',
+      icon: 'read',
+    },
+    {
+      title: 'Jugadores',
+      icon: 'user',
+    },
+    {
+      title: 'Tabla',
+      icon: 'table',
+    },
+    {
+      title: 'Torneos',
+      icon: 'trophy',
+    },
+  ];
 }
